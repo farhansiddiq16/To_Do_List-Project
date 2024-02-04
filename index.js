0c004bd820759b8e8ced3ba833747915b6ee9e57
Farhan Siddiq <157281829+farhansiddiq16@users.noreply.github.com>
2024-02-04 14:30:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+let todos = [];
+async function createTodo(todos) {
+    do {
+        let answer = await inquirer.prompt({
+            type: "list",
+            message: "Selet an operation",
+            name: "select",
+            choices: ["Add", "Update", "View", "Delete", "Exit"]
+        });
+        if (answer.select == "Add") {
+            let addtodo = await inquirer.prompt({
+                type: "input",
+                message: "Add item",
+                name: "todo"
+            });
+            todos.push(addtodo.todo);
+            console.log(todos);
+        }
+        if (answer.select == "Update") {
+            let updatetodo = await inquirer.prompt({
+                type: "list",
+                message: "Update item",
+                choices: todos.map(item => item),
+                name: "todo"
+            });
+            let addtodo = await inquirer.prompt({
+                type: "input",
+                message: "Add item",
+                name: "todo"
+            });
+            let newtodos = todos.filter(val => val !== updatetodo.todo);
+            todos = [...newtodos, addtodo.todo];
+            console.log(todos);
+        }
+        if (answer.select == "View") {
+            console.log(todos);
+        }
+        if (answer.select == "Delete") {
+            let deletetodo = await inquirer.prompt({
+                type: "list",
+                message: "Delete item",
+                choices: todos.map(item => item),
+                name: "todo"
+            });
+            let newtodos = todos.filter(val => val !== deletetodo.todo);
+            todos = [...newtodos];
+            console.log(todos);
+        }
+        if (answer.select == "Exit") {
+            break;
+        }
+    } while (true);
+}
+createTodo(todos);
